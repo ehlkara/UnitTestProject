369fcdc48bff22219a673a26ddd85d1bd4910a66
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-02-22 09:55:59 +0300

    basic unittest proccess

---
+++ UnitTest.Test/CalculatorTest.cs
@@ -0,0 +1,24 @@
+﻿using UnitTest.App;
+using Xunit;
+
+namespace UnitTest.Test
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void AddTest()
+        {
+            //Arrange
+
+            int a = 5;
+            int b = 20;
+            var calculator = new Calculator();
+
+            //Act
+            var total = calculator.add(a, b);
+
+            //Assert
+            Assert.Equal<int>(25, total);
+        }
+    }
+}
