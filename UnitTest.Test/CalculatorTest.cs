444e79071da70f4a5b63e7b186970c11e970533d
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-02-23 09:28:33 +0300

    builded DI service for test

--- UnitTest.Test/CalculatorTest.cs
+++ UnitTest.Test/CalculatorTest.cs
@@ -11,7 +11,7 @@ namespace UnitTest.Test
 
         public CalculatorTest()
         {
-            this.calculator = new Calculator();
+            this.calculator = null;
         }
 
         [Fact]
