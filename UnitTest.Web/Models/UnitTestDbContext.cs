8894ccbd0696942cb0ca4f52e28aba827cb7e0af
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-02-25 16:01:33 +0300

    added connection string

--- UnitTest.Web/Models/UnitTestDbContext.cs
+++ UnitTest.Web/Models/UnitTestDbContext.cs
@@ -19,15 +19,6 @@ namespace UnitTest.Web.Models
 
         public virtual DbSet<Product> Products { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-M7H0KCS;Initial Catalog=UnitTestDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
-            }
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Product>(entity =>
--- UnitTest.Web/Startup.cs
+++ UnitTest.Web/Startup.cs
@@ -1,13 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using UnitTest.Web.Models;
 
 namespace UnitTest.Web
 {
@@ -24,6 +21,8 @@ namespace UnitTest.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<UnitTestDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("SqlConStr")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
