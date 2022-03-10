f1c4b897cb30a98f048a857a9044e5249b8ae8bb
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-03-10 09:30:21 +0300

    tested index method

--- UnitTestWeb.Test/ProductControllerTest.cs
+++ UnitTestWeb.Test/ProductControllerTest.cs
@@ -1,4 +1,5 @@
-﻿using Moq;
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 using UnitTest.Web.Controllers;
 using UnitTest.Web.Models;
 using UnitTest.Web.Repository;
+using Xunit;
 
 namespace UnitTestWeb.Test
 {
@@ -24,6 +26,14 @@ namespace UnitTestWeb.Test
             _controller = new ProductsController(_mockRepo.Object);
 
             products = new List<Product>() { new Product { Id = 1, Name = "Pen", Price = 100, Stock = 50, Color = "Red" }, new Product { Id = 2, Name = "Notebook", Price = 200, Stock = 500, Color = "Blue" } };
+        }
+
+        [Fact]
+        public async void Index_ActionExecute_ReturnView()
+        {
+            var result = await _controller.Index();
+
+            Assert.IsType<ViewResult>(result);
         }
     }
 }
