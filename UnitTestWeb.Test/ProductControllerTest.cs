d23905ff29957294b7cadb0b7bc63e668ab82631
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-03-10 09:19:14 +0300

    created xUnit project for web project

---
+++ UnitTestWeb.Test/ProductControllerTest.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTest.Web.Controllers;
+using UnitTest.Web.Models;
+using UnitTest.Web.Repository;
+
+namespace UnitTestWeb.Test
+{
+    public class ProductControllerTest
+    {
+        private readonly Mock<IRepository<Product>> _mockRepo;
+
+        private readonly ProductsController _controller;
+
+        private List<Product> products;
+
+        public ProductControllerTest()
+        {
+            _mockRepo = new Mock<IRepository<Product>>();
+            _controller = new ProductsController(_mockRepo.Object);
+
+            products = new List<Product>() { new Product { Id = 1, Name = "Pen", Price = 100, Stock = 50, Color = "Red" }, new Product { Id = 2, Name = "Notebook", Price = 200, Stock = 500, Color = "Blue" } };
+        }
+    }
+}
