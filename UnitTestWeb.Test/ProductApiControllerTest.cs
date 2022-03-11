d9301f7b48006ee7d925364ac12398c3d72be174
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-03-11 11:36:35 +0300

    tested getProductById

--- UnitTestWeb.Test/ProductApiControllerTest.cs
+++ UnitTestWeb.Test/ProductApiControllerTest.cs
@@ -40,5 +40,18 @@ namespace UnitTestWeb.Test
 
             Assert.Equal<int>(2, returnProduct.ToList().Count);
         }
+
+        [Theory]
+        [InlineData(0)]
+        public async void GetProduct_IdInvalid_ReturnNotFound(int productId)
+        {
+            Product product = null;
+
+            _mockRepo.Setup(x => x.GetById(productId)).ReturnsAsync(product);
+
+            var result = await _controller.GetProduct(productId);
+
+            Assert.IsType<NotFoundResult>(result);
+        }
     }
 }
